5864f420220c283667d195dab482a3598b9a6025
Kenneth Balantucas <kennethbalantucas14@gmail.com>
2024-10-28 01:55:08 +0800

    Backend server
    I am adding a backend server to my app. Not yet setup properly

--- src/server.js
+++ src/server.js
@@ -1,19 +1,36 @@
 import express from "express";
-import bodyParser from "body-parser";
+// import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+console.log(app)
+
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.use(express.static("public"));
+
+// app.get("/", (req, res) => {
+//     res.render("home.ejs");
+// });
+
+// app.get("/login", (req, res) => {
+//     res.render("login.ejs");
+// });
+
+// app.get("/register", (req, res) => {
+//     res.render("register.ejs");
+// });
+
+// app.post("/register", async (req, res) => { });
+
+// app.post("/login", async (req, res) => { });
 
 
-app.get("/", (req, res) => {
-    res.render("home.ejs");
-});
 
-
-
+app.get('/', (req, res) => {
+    res.send('Hello from our server!')
+})
 
 
 
